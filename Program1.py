bc98833e92a721e09336c58c985d5b7ab5b60df6
Rathna08 <89316914+Rathna08@users.noreply.github.com>
2024-09-04 15:04:43 +0000

    done

--- Program1.py
+++ Program1.py
@@ -1,1 +1,93 @@
 
+# Getting started #
+#**Learning objectives**
+#
+#After this section:
+#
+#* You will have written and executed your first Python program
+#* You will know how to use the print command
+#* You know how to use arithmetic operations in python
+
+
+
+## Live Demo ##
+# print(yo)
+# print("yo")
+# print('yo')
+# print('"Come right back!", shouted the police officer.')
+#Live Demo:
+# print(8-10)
+# print(8+12*8)
+# print("8+12*8")
+# Show how to comment and why its important
+#print("Hours in a year:")
+# there are 365 days in a year and 24 hours in each day
+#print(365*24)
+#print("Hours in a year:")
+#print(365*24) # 365 days, 24 hours in each day
+
+
+
+## Problem 1 ##
+#Please write a script which prints out an emoticon: :-)
+
+print(" =| ")
+
+
+## Problem 2 ##
+#Pixar is a renound animation studio. 
+#This program is supposed to print out the names of some Pixar films in chronological order, 
+# but it's not working quite right yet. 
+# Please fix the program so that the names are printed in the correct order.
+
+print("Toy Story           - 1995")
+print("A Bug's Life        - 1998")
+print("Monsters, Inc.      - 2001")
+print("Finding Nemo        - 2003")
+print("The Incredibles     - 2004")
+print("Cars                - 2006")
+print("Ratatouille         - 2007")
+print("WALL-E              - 2008")
+print("Up                  - 2009")
+print("Inside Out          - 2015")
+print("Onward              - 2020")
+
+
+## Problem 3 ##
+#Please write a program which prints out the following lines exactly as they are written here, punctuation and all:
+#Sample output:
+#    Row, row, row your boat,
+#    Gently down the stream.
+#    Merrily, merrily, merrily, merrily,
+#    Life is but a dream.
+
+print("Row, row, row your boat,")
+print("Gently down the stream.")
+print("Merrily, merrily, merrily, merrily,")
+print("Life is but a dream.")
+
+
+## Problem 4 ##
+#Please write a script that prints out:
+# * Something that adds to 7
+# * Something that multiplies to 12
+# * Anything that uses multiple operations in one line
+
+print(5+2)
+print(12*1)
+print(7-3*9)
+
+## Problem 5 ##
+#Please write a script that prints out:
+# * The number of minutes in a year.
+
+print(60*24*365)
+
+## Problem 6 ##
+#Please write a script that prints out:
+# * 3 sentences
+# 	* 2 sentences should quotes from someone (like you're writing dialog for a book)
+
+print('“Hey!”')
+print('“What?”')
+print('“Where do you think you are going?”')
